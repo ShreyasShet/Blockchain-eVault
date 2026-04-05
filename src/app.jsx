e7e7e194fa959a386bb0422f53b74ded0e3385df
ShreyasShet <shreyasgshet79199@gmail.com>
2026-04-05 15:51:50 +0530

    init: add app component

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>LexVault</h1>
+      <p>Blockchain eVault for Legal Records</p>
+    </div>
+  )
+}
+
+export default App
